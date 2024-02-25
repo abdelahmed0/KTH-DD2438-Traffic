c6367c74cae93aa7c7531c45562c9f89da667397
Abdel Ahmed <abdu1999@gmx.net>
2024-02-25 19:08:35 +0100

    Added HRVO for the car

--- Assets/Scripts/AIP1TrafficCar.cs
+++ Assets/Scripts/AIP1TrafficCar.cs
@@ -33,6 +33,7 @@ public class AIP1TrafficCar : MonoBehaviour
     private int currentNodeIdx;
     private HybridAStarGenerator pathFinder = null;
     public bool drawDebug = false;
+    private float StuckTime = 1f; // Seconds after which to engage the backing up mechanism when stuck
 
     private Agent agent;
 
@@ -78,7 +79,9 @@ public class AIP1TrafficCar : MonoBehaviour
                 maxSpeed = m_Car.MaxSpeed, 
                 maxAngle = m_Car.m_MaximumSteerAngle,
                 allowReversing = allowReversing,
-                maxAccelaration = 6f
+                maxAccelaration = 6f,
+                Detector = m_Detector,
+                TimeLookAhead = 3f
             };
 
             StaticInitDone = true;
@@ -91,7 +94,11 @@ public class AIP1TrafficCar : MonoBehaviour
         // Generate path with Hybrid A*
         currentNodeIdx = 0;
         pathFinder = new HybridAStarGenerator(m_ObstacleMap.mapGrid, m_ObstacleMap, m_Car.m_MaximumSteerAngle, m_Collider,
-                        colliderResizeFactor, false, allowReversing, 2f);
+                        colliderResizeFactor, false, allowReversing, 2f)
+        {
+            Detector = m_Detector
+        };
+
         nodePath = pathFinder.GeneratePath(
             new Vector3(localStart.x, 0.05f, localStart.z),
             new Vector3(localGoal.x, 0.05f, localGoal.z),
@@ -103,7 +110,7 @@ public class AIP1TrafficCar : MonoBehaviour
         Vector3 old_wp = localStart;
         foreach (var wp in nodePath)
         {
-            Debug.DrawLine(m_ObstacleMap.mapGrid.LocalToWorld(old_wp), m_ObstacleMap.mapGrid.LocalToWorld(wp.LocalPosition), Color.green, 4f);
+            Debug.DrawLine(m_ObstacleMap.mapGrid.LocalToWorld(old_wp), m_ObstacleMap.mapGrid.LocalToWorld(wp.LocalPosition), Color.green, 2f);
             old_wp = wp.LocalPosition;
         }
 
@@ -115,74 +122,59 @@ public class AIP1TrafficCar : MonoBehaviour
 
     private void FixedUpdate()
     {
+        // TODO: Car collision avoidance
+        // TODO: VO RVO HRVO class refactoring
+        // TODO: Fix HRVO?
+
         if (nodePath.Count == 0)
         {
             return;
         }
-        
-        // TODO: Fix HRVO
-        // TODO: Vehicle stuck timer?
+        if (StuckTime <= 0f)
+        {
+            StuckTime = 1f;
+            currentNodeIdx = Math.Max(0, currentNodeIdx - 20);
+        }
+        else if (my_rigidbody.velocity.magnitude < 0.1f)
+            StuckTime -= Time.fixedDeltaTime;
 
-        AStarNode target = nodePath[currentNodeIdx];
-        AStarNode nextTarget = nodePath[Math.Min(currentNodeIdx + 1, nodePath.Count-1)];
-        Vector3 targetPosition = m_MapManager.grid.LocalToWorld(target.LocalPosition);
-        Vector3 nextTargetPosition = m_MapManager.grid.LocalToWorld(nextTarget.LocalPosition);
-        float targetSpeed = Mathf.Lerp(0f, m_Car.MaxSpeed, ((nextTargetPosition - targetPosition) / m_Car.MaxSpeed).magnitude);
-        targetVelocity = (nextTargetPosition - targetPosition).normalized * targetSpeed;
+        CalculateTargets(lookaheadDistance:20f, 
+            out Vector3 targetPosition, out Vector3 targetVelocity);
 
-        // Apply weighted avoidance via velocity obstacles
         float avoidanceRadius = colliderResizeFactor * m_Collider.transform.localScale.z;
         agent.Update(new Agent(Vec3To2(transform.position), Vec3To2(my_rigidbody.velocity), Vec3To2(targetVelocity), avoidanceRadius));
 
-        Vector2 newVelocity = voManager.CalculateNewRVOVelocity(agent, Time.fixedDeltaTime, out bool isColliding);
+        Vector2 newVelocity = voManager.CalculateNewHRVOVelocity(agent, Time.fixedDeltaTime, out bool isColliding);
 
-        float avoidanceWeight = isColliding ? 1f : 0f;
-        
-        Vector3 avoidanceVelocity = Vec2To3(newVelocity);
-        Vector3 avoidancePosition = transform.position + avoidanceVelocity.normalized * pathFinder.globalStepDistance;
+        if (isColliding)
+        {
+            // Avoid other agents if collision is detected via VO
+            Vector3 avoidanceVelocity = Vec2To3(newVelocity);
+            Vector3 avoidancePosition = transform.position + avoidanceVelocity;
 
-        PdControll(targetPosition * (1f - avoidanceWeight) + avoidancePosition * avoidanceWeight,
-                   targetVelocity * (1f - avoidanceWeight) + avoidanceVelocity * avoidanceWeight);
+            PdControll(avoidancePosition, avoidanceVelocity);
+            Debug.DrawLine(transform.position, transform.position + avoidanceVelocity, Color.green);
+        }
+        else
+        {
+            PdControll(targetPosition, targetVelocity);
+            Debug.DrawLine(transform.position, transform.position + targetVelocity, Color.blue);
+        }
 
-        Debug.DrawLine(transform.position, targetPosition, Color.magenta);
-        // Debug.DrawLine(transform.position, transform.position + my_rigidbody.velocity, Color.white);
-        Debug.DrawLine(transform.position, transform.position + targetVelocity, Color.blue);
-        Debug.DrawLine(transform.position, transform.position + avoidanceVelocity, Color.green);
-        // Debug.DrawLine(transform.position, avoidancePosition, Color.red);
+        // Debug.DrawLine(transform.position, targetPosition, Color.magenta);
+        // Debug.DrawLine(transform.position, transform.position + my_rigidbody.velocity, Color.black);
+
     }
 
     private void PdControll(Vector3 targetPosition, Vector3 targetVelocity)
     {
         Vector3 current_position = transform.position;
-        
-        // Number nodes to lookahead to see if path is turning
-        int lookahead = 2;
-        int lookahead_index = Mathf.Min(currentNodeIdx + lookahead, nodePath.Count - 1);
-
-        Vector3 current_direction = transform.forward;
-        Vector3 lookahead_position = nodePath[lookahead_index].GetGlobalPosition();
-        Vector3 direction_to_lookahead_index = current_position - lookahead_position;
-
-        float angle_between_vectors = Vector3.Angle(current_direction.normalized, direction_to_lookahead_index.normalized);
-
-        // A threshold when we consider a checkpoint reached
-        float reach_threshold = 9f;
-
-        reach_threshold *= angle_between_vectors / 180.0f;
-
-
-        Vector3 pos_error = targetPosition - current_position;
-        Vector3 vel_error = targetVelocity - my_rigidbody.velocity;
-        Vector3 desired_acceleration = (k_p * pos_error + k_d * vel_error).normalized; // normalize to receive steering and accel values between (-1, 1)
-
-        float steering = Vector3.Dot(desired_acceleration, transform.right);
-        float acceleration = Vector3.Dot(desired_acceleration, transform.forward);
 
         float distance = Vector3.Distance(targetPosition, current_position);
-        if (distance < reach_threshold || distance < 3f)
+        if (distance < 2f)
         {
             currentNodeIdx = Mathf.Min(currentNodeIdx + 1, nodePath.Count - 1);
-            targetPosition = nodePath[currentNodeIdx].GetGlobalPosition();
+            return;
         }
 
         for (int i = currentNodeIdx + 1; i < nodePath.Count; ++i)
@@ -192,13 +184,88 @@ public class AIP1TrafficCar : MonoBehaviour
             
             if (current_tracked_distance > potential_tracked_distance)
             {
-                // found a closer point, i, no need to keep looking
+                // found a closer point
                 currentNodeIdx = i;
-                break; 
+                return;
             }
         }
+        
+        // Move via PD controller
+        Vector3 pos_error = targetPosition - current_position;
+        Vector3 vel_error = targetVelocity - my_rigidbody.velocity;
+        Vector3 desired_acceleration = (k_p * pos_error + k_d * vel_error).normalized; // normalize to receive steering and accel values between (-1, 1)
+
+        float steering = Vector3.Dot(desired_acceleration, transform.right);
+        float acceleration = Vector3.Dot(desired_acceleration, transform.forward);
 
         m_Car.Move(steering, acceleration, acceleration, 0f);
+    }
+
+    private void CalculateTargets(float lookaheadDistance, out Vector3 targetPosition, out Vector3 targetVelocity)
+    {
+        AStarNode target = nodePath[currentNodeIdx];
+        targetPosition = m_MapManager.grid.LocalToWorld(target.LocalPosition);
+
+        Vector3 lookaheadPosition = PurePursuitTargetPosition(lookaheadDistance);
+        Vector2 lookaheadPosition2d = new(lookaheadPosition.x, lookaheadPosition.z);
+
+        Vector2 position2d = new(transform.position.x, transform.position.z);
+
+        // Only use lookahead if it is not occluded
+        if(!m_Detector.LineCollision(position2d, lookaheadPosition2d))
+        {
+            Vector2 currentDirection = new(my_rigidbody.velocity.normalized.x, my_rigidbody.velocity.normalized.z);
+            Vector2 directionToLookhead = (lookaheadPosition2d - position2d).normalized;
+
+            float lookaheadAngle = Vector2.Angle(directionToLookhead, currentDirection);
+            float targetSpeed = Mathf.Lerp(0f, m_Car.MaxSpeed, 1f - Mathf.Clamp01(lookaheadAngle / 60f));
+            targetVelocity = (lookaheadPosition - targetPosition).normalized * targetSpeed;
+        }
+        else 
+        { 
+            AStarNode nextTarget = nodePath[Math.Min(currentNodeIdx + 1, nodePath.Count-1)];
+            Vector3 nextTargetPosition = m_MapManager.grid.LocalToWorld(nextTarget.LocalPosition);
+
+            float targetSpeed = Mathf.Lerp(0f, m_Car.MaxSpeed, ((nextTargetPosition - targetPosition) / m_Car.MaxSpeed).magnitude);
+            targetVelocity = (nextTargetPosition - targetPosition).normalized * targetSpeed;
+        }
+    }
+
+    private Vector3 PurePursuitTargetPosition(float lookaheadDistance)
+    {
+        for (int i = currentNodeIdx; i < nodePath.Count; i++)
+        {
+            Vector3 pathNodePosition = nodePath[i].GetGlobalPosition();
+            if (Vector3.Distance(transform.position, pathNodePosition) > lookaheadDistance)
+            {
+                // Debug.DrawLine(transform.position, pathNodePosition, Color.white);
+                return pathNodePosition;
+            }
+        }
+        return nodePath[^1].GetGlobalPosition(); // Return last node if none found within lookahead distance
+    }
+
+    private void OnDrawGizmos() {
+        if (drawDebug)
+        {
+            voManager?.DebugDraw(agent);
+            // m_Detector?.DebugDrawBoundingBoxes();
+        }
+        // if (drawDebug && pathFinder != null && pathFinder.flowField != null)
+        // {
+        //     foreach (var posEntity in pathFinder.flowField)
+        //     {
+        //         var cell = new Vector3Int(posEntity.Key.x, posEntity.Key.y, 1);
+        //         var cellGlobal = m_MapManager.grid.CellToWorld(cell);
+
+        //         var gizmoSize = m_MapManager.grid.cellSize;
+        //         gizmoSize.y = 0.005f;
+        //         gizmoSize.Scale(m_MapManager.grid.transform.localScale * 0.8f);
+                
+        //         Gizmos.color = Mathf.CorrelatedColorTemperatureToRGB(1000f + 1000f * posEntity.Value);
+        //         Gizmos.DrawCube(cellGlobal, gizmoSize);
+        //     }
+        // }
     }
 
     private Vector2 Vec3To2(Vector3 vec)
@@ -209,24 +276,5 @@ public class AIP1TrafficCar : MonoBehaviour
     private Vector3 Vec2To3(Vector2 vec)
     {
         return new Vector3(vec.x, 0f, vec.y);
-    }
-
-    void OnDrawGizmos()
-    {
-        if (drawDebug && pathFinder != null && pathFinder.flowField != null)
-        {
-            foreach (var posEntity in pathFinder.flowField)
-            {
-                var cell = new Vector3Int(posEntity.Key.x, posEntity.Key.y, 1);
-                var cellGlobal = m_MapManager.grid.CellToWorld(cell);
-
-                var gizmoSize = m_MapManager.grid.cellSize;
-                gizmoSize.y = 0.005f;
-                gizmoSize.Scale(m_MapManager.grid.transform.localScale * 0.8f);
-                
-                Gizmos.color = Mathf.CorrelatedColorTemperatureToRGB(1000f + 1000f * posEntity.Value);
-                Gizmos.DrawCube(cellGlobal, gizmoSize);
-            }
-        }
     }
 }
